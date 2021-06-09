e0e4848d605918e7147260b329b9f140ae7e8af0
Adriana Carollo <adri@ASUS.com>
2021-06-09 17:13:53 +0200

    Creato form registrazione (solo html)

---
+++ forms/registrato.php
@@ -0,0 +1,9 @@
+<?php
+
+    $nome = $_POST["name"];
+    $cognome  = $_POST["surname"];
+    $data_nascita = $_POST["birthdate"];
+    $email = $_POST["email"];
+    $pass = $_POST["password"];
+    $conf_pass = $_POST["confermaPassword"];
+?>
