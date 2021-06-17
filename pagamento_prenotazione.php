8af471c742d6168de401e3772272464b030cb217
Adriana Carollo <adri@ASUS.com>
2021-06-17 15:07:28 +0200

    pagamento prenotazione (non collegato a prenotazione, solo default)

---
+++ pagamento_prenotazione.php
@@ -0,0 +1,145 @@
+<?php
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>LaMantia - Home</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <!-- TODO: Trova una nuova favicon -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    <!-- =======================================================
+  * Template Name: Restaurantly - v3.3.0
+  * Template URL: https://bootstrapmade.com/restaurantly-restaurant-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+    <!-- ======= Top Bar ======= -->
+    <div id="topbar" class="d-flex align-items-center sticky-top">
+        <div class="container d-flex justify-content-center justify-content-md-between">
+
+            <div class="contact-info d-flex align-items-center">
+                <!--
+        <i class="bi bi-phone d-flex align-items-center"><span>+1 5589 55488 55</span></i>
+      -->
+                <i class="bi bi-clock d-flex align-items-center ms-4"><span> Lun-Sab: 12.00 - 21.00</span></i>
+            </div>
+
+            <!--
+      <div class="languages d-none d-md-flex align-items-center">
+        <ul>
+          <li>En</li>
+          <li><a href="#">De</a></li>
+        </ul>
+      </div>
+    -->
+        </div>
+    </div>
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top d-flex align-items-cente">
+        <div class="container-fluid container-xl d-flex align-items-center justify-content-lg-between">
+
+            <h1 class="logo me-auto me-lg-0"><a href="index.php">LaMantia</a></h1>
+            <!-- Uncomment below if you prefer to use an image logo -->
+            <!-- <a href="index.php" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+            <nav id="navbar" class="navbar order-last order-lg-0">
+                <ul>
+                    <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+                    <li><a class="nav-link scrollto" href="#about">Chi siamo</a></li>
+                    <li><a class="nav-link scrollto" href="#menu">Menu</a></li>
+                    <li><a class="nav-link scrollto" href="#specials">Servizi</a></li>
+                    <li><a class="nav-link scrollto" href="#events">Eventi</a></li>
+                    <li><a class="nav-link scrollto" href="#chefs">Chefs</a></li>
+                    <li><a class="nav-link scrollto" href="#gallery">Galleria</a></li>
+                    <li><a class="nav-link" href="Ristoranti.php">Dove siamo</a></li>
+                    <li><a class="nav-link scrollto" href="#contact">Contattaci</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+            <?php
+            if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+            ?>
+                <a href="index.php#book-a-table" class="book-a-table-btn">Prenota</a>
+                <a href="logout.php" class="book-a-table-btn">Logout</a>
+            <?php
+            } else {
+            ?>
+
+                <a href="login.php" class="book-a-table-btn">Login</a>
+                <a href="registrazione.php" clas="book-a-table-btn">Registrati</a>
+            <?php
+            }
+            ?>
+            <!--<a href = "#book-a-table" class="book-a-table-btn scrollto d-none d-lg-flex">Book a table</a> -->
+
+        </div>
+    </header><!-- End Header -->
+
+    <section id="paypal-button-container">
+        <!-- Script Paypal API -->
+        <!-- Script con authorize 
+          <script src="https://www.paypal.com/sdk/js?client-id=AY7aAfew0L4yha9Uihrw0tqORdnPRpLaSokGVo-nYozreGlfQS-tUNXC67AkV3tkV8fq7Ll0CAK_ayo3&currency=EUR&intent=authorize"></script>
+         -->
+         <script src="https://www.paypal.com/sdk/js?client-id=AY7aAfew0L4yha9Uihrw0tqORdnPRpLaSokGVo-nYozreGlfQS-tUNXC67AkV3tkV8fq7Ll0CAK_ayo3&currency=EUR"></script>
+
+        <script>
+  paypal.Buttons({
+    createOrder: function(data, actions) {
+      // This function sets up the details of the transaction, including the amount and line item details.
+      return actions.order.create({
+        purchase_units: [{
+          amount: {
+            value: '0.01'
+          }
+        }]
+      });
+    },
+    onApprove: function(data, actions) {
+      // This function captures the funds from the transaction.
+      return actions.order.capture().then(function(details) {
+        // This function shows a transaction success message to your buyer.
+        //alert('Transaction completed by ' + details.payer.name.given_name);
+        console.log(details);
+      });
+    }
+  }).render('#paypal-button-container');
+  //This function displays Smart Payment Buttons on your web page.
+</script>
+    </section>
+</body>
+
+
+
+</html>
